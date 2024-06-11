fbd53e073014415aa133ca3665b8391a2d4cf33c
Fdrodriguez1234 <faru534628@gmail.com>
2024-06-11 14:54:46 -0500

    update hola.css
    hice el.register

--- hola.js
+++ hola.js
@@ -3,7 +3,7 @@ let loginId = document.getElementById('login');
 let registerId = document.getElementById('register');
 
 btnId.addEventListener('click',() =>{
-    if(loginId.style.display == 'block'){
+    if(registerId.style.display == 'none'){
         loginId.style.display = 'none';
         registerId.style.display = 'block';
     }
